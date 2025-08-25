9cfe31d1ef5b1a8a67501b7ef5c7602187109105
HichamHanane <hichamhanane2000@gmail.com>
2025-08-25 16:26:18 +0200

    add user model

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+const User = require('./Models/User');
 mongoose.connect(process.env.DB_URL)
 .then(
     console.log('database connected successfully...')
