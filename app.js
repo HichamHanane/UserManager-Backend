8a7f0c10c94c586c43e5d6e136b501a2f1c1f439
HichamHanane <hichamhanane2000@gmail.com>
2025-08-25 16:18:27 +0200

    connect to database mongoAtlas

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect(process.env.DB_URL)
+.then(
+    console.log('database connected successfully...')
+    
+).catch((error)=>{
+    console.log('Error while connecting to database : ',error);
+    
+})
 
 
 
